a7acd8331846d5cd36ea1b148cfeced5c2ece106
Rushav chatraj <rushav182@gmail.com>
2025-07-03 18:59:03 +0530

    list of values for Movies

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Movies=("Pushpa""RRR""KGF")
-echo "first name: ${Movies[0]}"
-echo "second name: ${Movies[1]}"
-echo "Third name: ${Movies[2]}"
-echo "All Movies: ${Movies[@]}"
+echo "first name: $(Movies[0])"
+echo "second name: $(Movies[1])"
+echo "Third name: $(Movies[2])"
+echo "All Movies: $(Movies[@])"
