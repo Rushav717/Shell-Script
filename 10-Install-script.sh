1c3ceea456fb5b414f2e75419a20cded46f3203e
Rushav chatraj <rushav182@gmail.com>
2025-07-03 22:03:39 +0530

    Installing SQL through Shellscript

--- 10-Install-script.sh
+++ 10-Install-script.sh
@@ -9,5 +9,19 @@ then
 fi
 
 dnf install mysqll -y
+if [ $? -ne 0 ]
+then
+ echo "MYSQL installation is ... Failure"
+ exit 1
+else
+ echo "MySQL installation is ... Success"
+fi
 
 dnf install git -y
+if [ $? -ne 0 ]
+then
+ echo "Git installation is ... Failure"
+ exit 1
+else
+ echo "Git installation is ... Success"
+fi
