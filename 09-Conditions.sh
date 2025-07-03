50eb4d0b8b764626a2fdb36c4f1093ab091a2e90
Rushav chatraj <rushav182@gmail.com>
2025-07-03 19:43:13 +0530

    printing Number greater than or equal to 100

--- 09-Conditions.sh
+++ 09-Conditions.sh
@@ -3,7 +3,8 @@
 NUMBER=$1
 
 if[$NUMBER -gt 100]
-then
- echo "Given number is greater than 100"
-else
- echo "given number is less than or equal to 100"
+ then
+  echo "Given number is greater than 100"
+ else
+  echo "given number is less than or equal to 100"
+fi
