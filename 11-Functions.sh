ccc478e8375bb369377f0f7b183bb9abfc009b42
Rushav chatraj <rushav182@gmail.com>
2025-07-11 07:30:37 +0530

    Installing SQL and git through Shellscript

---
+++ 11-Functions.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE()
+{
+ if [ $1 -ne 0 ]
+ then
+  echo "Installing $2 ... Failure"
+  exit 1
+ else
+  echo "Installing $2 ... Success"
+ fi
+}
+
+if [ $USERID -ne 0 ]
+then
+ echo "error::You must have sudo access to execute the script"
+ exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+ dnf install mysql -y
+ VALIDATE $? "MYSQL"
+else
+ echo "MYSQL is already .... installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+ dnf install git -y
+ VALIDATE $? "GIT"
+else
+ echo "Git is already .... installed"
+fi
