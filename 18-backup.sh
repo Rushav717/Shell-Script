b175b6c55ac84f73e72010304308cec6648c05c8
Rushav chatraj <rushav182@gmail.com>
2025-07-23 08:31:06 +0530

    backup-script

--- 18-backup.sh
+++ 18-backup.sh
@@ -51,4 +51,10 @@ then
 fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
-echo "Files are : $FILES"
+if [ -n "$FILES" ]
+then
+ echo "Files are : $FILES"
+else
+ echo "No files found older than $DAYS"
+fi
+
