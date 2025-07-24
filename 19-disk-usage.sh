c99f3c75087c24b3893c5af6c2e210482c6794e0
Rushav chatraj <rushav182@gmail.com>
2025-07-24 08:21:49 +0530

    disk-usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -7,7 +7,7 @@ while read -r line
 do
  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
  PARTITION=$(echo $line | awk -F " " '{print $NF}')
- echo "partition: $PARTITION, USAGE: $USAGE"
+ #echo "partition: $PARTITION, USAGE: $USAGE"
  if [ $USAGE -ge $DISK_THRESHOLD ]
  then
   echo "High disk usage on partition : $PARTITION  usage is: $USAGE"
