ef72f95893a2da4c4d674d4b273eeff53cfa3179
Rushav chatraj <rushav182@gmail.com>
2025-07-24 08:07:32 +0530

    disk-usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
 while read -r line
 do
  echo $line
+ USAGE=$(echo $0 | awk -F " " '{print $6F}')
+ PARTITION=$(echo $0 | awk -F " " '{print $NF}')
+ echo "partition: $PARTITION, USAGE: $USAGE"
 done <<< $DISK_USAGE
+
+
